7d42a4d744e9c95dd0f5c5dabb5788c4be1f64f4
Ilu <ilu@twilight.local>
2020-12-06 10:40:42 +0100

    day 6

---
+++ d6/d6_1.js
@@ -0,0 +1,52 @@
+var lines;
+var data = [];
+
+var questions = "abcdefghijklmnopqrstuvwxyz".split("");
+
+function Setup() {
+	var input = Read("d6/input.txt");
+	lines = input.trim().split("\n");
+}
+
+function Loop() {
+	var current = { size: 0 };
+	for (var i = 0; i < lines.length; i++) {
+		var l = lines[i].trim();
+		if (l) {
+			current['size']++;
+			var parts = l.split("");
+			for (var j = 0; j < parts.length; j++) {
+				if (parts[j] in current) {
+					current[parts[j]]++;
+				} else {
+					current[parts[j]] = 1;
+				}
+			}
+		} else {
+			data.push(current);
+			current = { size: 0 };
+		}
+	}
+	data.push(current);
+
+	Println("Groups=" + data.length);
+
+	var sum = 0;
+	for (var j = 0; j < data.length; j++) {
+		var group = data[j];
+		var q = 0;
+		for (var i = 0; i < questions.length; i++) {
+			if (questions[i] in group) {
+				q++;
+			}
+		}
+		//Println("Group #" + j + " := " + q);
+		sum += q;
+	}
+	Println("result=" + sum);
+
+	Stop();
+}
+
+function Input(e) { }
+
